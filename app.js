1cf83df95d915a60002c5886ce75d6417452bb80
hhjung1202 <hhjung1202@naver.com>
2016-09-23 20:24:50 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// function mapping(item, isParent){
+// 	var parent = document.getElementById('parentDiv');
+// 	if(isParent !== "parent"){
+// 		parent = isParent;
+// 	}
+// 	if(Array.isArray(item)){
+// 		for(var i in item){
+// 			parent.appendChild(item);
+// 		}
+// 	}else{
+// 		parent.appendChild(item);
+// 	}
+// }
+// mapping(p, div);
+// mapping(p2, div);
+// mapping(div, "parent");
